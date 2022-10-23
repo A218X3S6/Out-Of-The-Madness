b3b7ee085e7ce0843783a3fc77bbb3a26b2124fc
Rafał Rółkowski <roleq2233@gmail.com>
2022-10-23 18:34:42 +0200

    Second commit
    - build demo

--- Out of the Madness/Build demo 0.1v/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_debugger.cpp
+++
@@ -1,2 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\roleq\Out of the Madness\Build demo 0.1v\Il2CppOutputProject\IL2CPP\libil2cpp\debugger\il2cpp-stubs.cpp"
--- Out of the Madness/Build demo 0.1v/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_gc.cpp
+++
@@ -1,7 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\roleq\Out of the Madness\Build demo 0.1v\Il2CppOutputProject\IL2CPP\libil2cpp\gc\BoehmGC.cpp"
-#include "C:\Users\roleq\Out of the Madness\Build demo 0.1v\Il2CppOutputProject\IL2CPP\libil2cpp\gc\GCHandle.cpp"
-#include "C:\Users\roleq\Out of the Madness\Build demo 0.1v\Il2CppOutputProject\IL2CPP\libil2cpp\gc\GarbageCollector.cpp"
-#include "C:\Users\roleq\Out of the Madness\Build demo 0.1v\Il2CppOutputProject\IL2CPP\libil2cpp\gc\NullGC.cpp"
-#include "C:\Users\roleq\Out of the Madness\Build demo 0.1v\Il2CppOutputProject\IL2CPP\libil2cpp\gc\WriteBarrier.cpp"
-#include "C:\Users\roleq\Out of the Madness\Build demo 0.1v\Il2CppOutputProject\IL2CPP\libil2cpp\gc\WriteBarrierValidation.cpp"
